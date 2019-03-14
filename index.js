d4b490d19fb3cefca3962eff3cc12d1f3ee81d9f
shouko <imshouko@gmail.com>
2019-03-14 14:52:03 +0900

    initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const rp = require('request-promise')
+
+const baseUrl = 'http://blog.nogizaka46.com/?p='
+const apiUrl = 'https://apis.zaka46.tw/dcimg/'
+
+Promise.all([1, 2, 3, 4, 5, 6, 7].map(function(e) {
+    return rp(`${baseUrl}${e}`)
+})).then(function(res) {
+    var resText = res.join()
+    var links = []
+    var re = /<a href="http:\/\/dcimg.awalker.jp\/view\/([^\"]+)">/g
+    var m
+    while (m = re.exec(resText)) {
+        links.push(m[1])
+    }
+    return Promise.all(links.map(function(e) {
+        return rp(`${apiUrl}${e}`)
+    }))
+}).then(function(res) {
+    console.log('finished', res.length, 'requests')
+})
